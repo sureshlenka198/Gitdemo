d49ad5cf139e730c6a37cb74d9aeac3b40a2caa3
Sam <sureshlenka199805@gmail.com>
2024-07-18 17:30:07 +0530

    commited Javapro

---
+++ Javapro/src/pack/test.java
@@ -0,0 +1,12 @@
+package pack;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Line 1");
+		System.out.println("Hello Line 2");
+	}
+
+}
