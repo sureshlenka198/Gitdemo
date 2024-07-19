8799fee64a45ef448f142fc4fabe82cd4960a2aa
Sam <sureshlenka199805@gmail.com>
2024-07-19 17:42:30 +0530

    push

--- Javapro/src/pack/test3.java
+++ Javapro/src/pack/test3.java
@@ -8,24 +8,9 @@ public class test3 {
 		
 
 		System.out.println("Hello Line 11");
-		System.out.println("Hello Line 111");
-		
-		System.out.println("Hello Line 22");
-		System.out.println("Hello Line 222");
-        
-		
-		
-		
-		
-		//Added row
+		System.out.println("Hello Line 22");	
 		System.out.println("Hello Line 33");
 		
-		
-		//Added row
-		System.out.println("Hello Line 44");
-		
-		//Added row
-	   System.out.println("Hello Line 55");
 				
 		
 
