eeef6394cb62e27e81e851107f409c3406bc4760
Sam <sureshlenka199805@gmail.com>
2024-07-19 15:49:24 +0530

    Test 3 added in gitdemo

---
+++ Javapro/src/pack/test3.java
@@ -0,0 +1,13 @@
+package pack;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Line 1");
+		System.out.println("Hello Line 2");
+
+	}
+
+}
